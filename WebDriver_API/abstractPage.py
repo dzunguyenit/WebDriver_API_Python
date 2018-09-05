0d7e0e593a9e4248723dccd27bdb5d7d20a5597c
vunguyen <vu.nguyen@mediastep.com>
2018-09-05 12:57:18 +0700

    update

--- WebDriver_API/abstractPage.py
+++ WebDriver_API/abstractPage.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import random
 
 driver = webdriver.Chrome("E:\Python WebDriver\Ex1\WebdriverAPI\Driver\chromedriver.exe")
-driver.get("http://demo.guru99.com/v4/")
-driver.maximize_window()
-urlHomeBank = driver.current_url
 
-randomEmail = "vu" + "@gmail.com"
-#+ str(random.randint(1,99999999))
 
-driver.find_element_by_xpath("//a[contains(text(),'here')]").click()
-driver.find_element_by_xpath("//input[@name='emailid']").send_keys(randomEmail)
+class commonFunction:
+    def openurl(self, url):
+        driver.get(url)
+        driver.maximize_window()
 
-# assert "Python1" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-driver.quit()
+    def click(self, locator):
+        driver.find_element_by_xpath(locator).click()
+
+    def quitBrowser(self):
+        driver.quit()
+
+    def sendKeysToElement(self, locator, value):
+        driver.find_element_by_xpath(locator).send_keys(value)
