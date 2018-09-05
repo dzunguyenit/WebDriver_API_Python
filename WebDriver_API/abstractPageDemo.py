dcd1544607e1c20b84bba4c44c126afce60311c3
vunguyen <vu.nguyen@mediastep.com>
2018-09-05 15:34:31 +0700

    update

--- WebDriver_API/abstractPageDemo.py
+++
@@ -1,8 +0,0 @@
-from WebDriver_API.abstractPage import commonFunction
-
-abstract = commonFunction()
-abstract.openurl("http://demo.guru99.com/v4/")
-abstract.clickToElement("//a[contains(text(),'here')]")
-
-abstract.sendKeysToElement("//input[@name='emailid']", "nguyenene")
-abstract.quitBrowser()
